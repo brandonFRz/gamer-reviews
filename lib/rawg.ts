d986cef69be7c3b26f6af048f7a242488666b415
Busterblades <54655414+Busterblades@users.noreply.github.com>
2024-09-11 12:05:49 -0600

    replace axio

--- lib/rawg.ts
+++ lib/rawg.ts
@@ -1,45 +1,38 @@
 // lib/rawg.ts
-import axios from "axios";
-
 const RAWG_API_KEY = process.env.NEXT_PUBLIC_RAWG_API_KEY; // Guarda tu API key en las variables de entorno
+const BASE_URL = "https://api.rawg.io/api";
 
-// Configura una instancia de axios para interactuar con la API de RAWG
-const rawgApi = axios.create({
-  baseURL: "https://api.rawg.io/api",
-  params: {
-    key: RAWG_API_KEY,
-  },
-});
+// Función para hacer solicitudes GET genéricas con fetch
+async function fetchFromRawg(endpoint:string) {
+  const url = `${BASE_URL}${endpoint}?key=${RAWG_API_KEY}`;
+  try{
+    const response = await fetch(url)
+    if(!response.ok){
+      throw new Error(`Error en la solicitud: ${response.statusText}`)
+    }
+
+  const data = await response.json()
+  return data;
+
+  }catch(error){
+    console.error("Error fetching the data from RAWG API: ", error)
+  }
+}
 
 // Obtiene los detalles de un juego específico usando el slug.
 export async function getGameDetails(slug: string) {
-  try {
-    const { data } = await rawgApi.get(`/games/${slug}`);
-    return data;
-  } catch (error) {
-    console.error("Error fetching the details.", error);
-    return null;
-  }
+  return await fetchFromRawg(`/games/${slug}`);
 }
 
 // Obtiene las capturas de pantalla de un juego específico usando el slug.
 export async function getGameScreenshots(slug: string) {
-  try {
-    const { data } = await rawgApi.get(`/games/${slug}/screenshots`);
-    return data;
-  } catch (error) {
-    console.log("Error fetching the screenshots", error);
-    return { results: [] };
-  }
+  const data = await fetchFromRawg(`/games/${slug}/screenshots`);
+  return data || { results: [] };
 }
 
 // Obtiene los videos de un juego específico usando el slug.
 // export async function getGameVideos(slug: string) {
-//   try {
-//     const { data } = await rawgApi.get(`/games/${slug}/movies`);
-//     return data;
-//   } catch (error) {
-//     console.log("Error fetching the videos", error);
-//     return { results: [] };
-//   }
+//   const data = await fetchFromRawg(`/games/${slug}/movies`);
+//   return data || { results: [] };
 // }
+
